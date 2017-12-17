aa97991cb3487b5537bf05fe9ed190b919374317
Tomas Frohawk <tom@email.com>
2017-12-17 17:11:35 +0000

    Minor commit

--- looping.java
+++ looping.java
@@ -5,12 +5,18 @@ public class looping {
 		int count = 0;
 		// do while looping
 		do {
-			if (count > 100) {
-					check = false;
-			}
 			
+			// check if 'count' variable is more than 100, if it is, set 'check' to false.
+			if (count > 100) {
+				// set 'check' to false.
+				check = false;
+			}
+			// print 'count' variable value to console.
 			System.out.println(count);
+			
+			// increment 'count' variable by 1.
 			count++;
+			
 		} while (check == true);
 	}
 }
