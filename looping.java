69404159f4cdfa491c988b05ef75d2a7894c6933
Tomas Frohawk <tom@email.com>
2017-12-17 17:10:07 +0000

    Minor commit

--- looping.java
+++ looping.java
@@ -1,5 +1,16 @@
 public class looping {
 	public static void main(String args[]) {
 		System.out.println("Hello World!");
+		boolean check = true;
+		int count = 0;
+		// do while looping
+		do {
+			if (count > 100) {
+					check = false;
+			}
+			
+			System.out.println(count);
+			count++;
+		} while (check == true);
 	}
 }
